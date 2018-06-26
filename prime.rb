e66cd026514dcfbca58a9b0ce935e8a544fa87c0
Dina Hafez <dinahafez.8@gmail.com>
2018-06-26 22:24:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,3 @@
-require 'pry'
-
-# Add  code here!
-# def prime?(num)
-#   range = (2..num).to_a
-#   if num < 2
-#     false
-#   else 
-#     range.each do |i|
-#       if num % i == 0
-#         false
-#       end
-#     end
-#   end
-# end
 
 def prime?(num)
   range = (2..num-1).to_a
