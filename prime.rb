43c55d226f1bade98714508ba61952b508963752
Dina Hafez <dinahafez.8@gmail.com>
2018-06-26 21:47:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,11 @@ def prime?(num)
   range = (2..num).to_a
   if num < 2
     false
-  end
-  range.each do |i|
-    if num % i == 0
-      false
+  else 
+    range.each do |i|
+      if num % i == 0
+        false
+      end
     end
   end
   true
