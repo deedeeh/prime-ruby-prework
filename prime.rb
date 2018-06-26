7c79c847fe42ebc532b3a3d8daf818dea08bd4d5
Dina Hafez <dinahafez.8@gmail.com>
2018-06-26 23:16:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,16 @@
 
-def prime?(num)
-  range = (2..num-1).to_a
-  is_prime = true
-  range.each do |i|
-    if num % i == 0 
-      is_prime = false
-    end
-  end
-  if num < 2
-    is_prime = false
-  end
-  return is_prime
-end
+# def prime?(num)
+#   range = (2..num-1).to_a
+#   is_prime = true
+#   range.each do |i|
+#     if num % i == 0 
+#       is_prime = false
+#     end
+#   end
+#   if num < 2
+#     is_prime = false
+#   end
+#   return is_prime
+# end
 
 
